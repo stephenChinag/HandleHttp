ae3a584957c40cdff42bb587b3647c25c54fba6c
stephenChinag <stephenchinag@gmail.com>
2022-08-19 03:10:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,25 +7,38 @@ import './App.css';
 function App() {
 const [movies, setMovies]= useState([])
 const [isLoading ,setIsLoading]= useState(false)
-
+const [error ,setError]=useState(null)
 
  async function fetchMovieHandler (){
   setIsLoading(true)
+  setError(null)
+  try{
   const response = await fetch('https://swapi.dev/api/films' )
+ 
+ 
+  if(!response.ok){
+    throw new Error("Sorry Something went Wrong!")
+  }
   const data = await response.json()
+
+
     const transformMovies = data.results.map(movieData=> {
       return {
         id : movieData.episone_id,
         releaseDate:movieData.release_date,
         title:movieData.title,
-        openingText:movieData.opening_crawl
+        openingText:movieData.opening_crawl 
       }
 
     })
    setMovies(transformMovies)
-   setIsLoading(false)
-  
-
+   
+  }
+  catch(error){
+    setError(error.message)
+   
+  }
+  setIsLoading(false)
 
  }
   return (
@@ -35,7 +48,8 @@ const [isLoading ,setIsLoading]= useState(false)
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading &&  movies.length===0 && <p> no Movies Available Or try refresh buy clicking the Fetch movie button </p>}
+        {!isLoading &&  movies.length===0 && !error &&  <p> no Movies Available Or try refresh buy clicking the </p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
---
+++ src/components/Spin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Spin.module.css'
+
+
+const Spin=()=>{
+
+    return (
+        <div className={styles.loading }>
+
+        </div>
+    )
+
+}
+
+export default Spin;

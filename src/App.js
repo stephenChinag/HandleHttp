fc986b551f55c124dd95c04b48c476e3a5509d6e
stephenChinag <stephenchinag@gmail.com>
2022-08-19 01:32:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
 const [movies, setMovies]= useState([])
 
+
  function fetchMovieHandler (){
   fetch('https://swapi.dev/api/films' ).then(respone=>{
     return respone.json();
@@ -14,6 +15,7 @@ const [movies, setMovies]= useState([])
     const transformMovies = data.results.map(movieData=> {
       return {
         id : movieData.episone_id,
+        releaseDate:movieData.release_date,
         title:movieData.title,
         openingText:movieData.opening_crawl
       }

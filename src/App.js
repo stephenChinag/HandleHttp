57cface937ae4bab55305aa33186b52c984ff9dd
stephenChinag <stephenchinag@gmail.com>
2022-08-20 05:52:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ const [error ,setError]=useState(null)
   setIsLoading(true)
   setError(null)
   try{
-  const response = await fetch('https://swapi.dev/api/films' )
- 
+  const response = await fetch('https://http-a174f-default-rtdb.firebaseio.com/movies.json' )
  
   if(!response.ok){
     throw new Error("Sorry Something went Wrong!")
@@ -50,9 +49,19 @@ const [error ,setError]=useState(null)
 
 }, [fetchMovieHandler])
 
-const addMovieHandler =(movie)=>
-{
-  console.log(movies)
+async function addMovieHandler(movie){
+
+
+const response = await fetch('https://http-a174f-default-rtdb.firebaseio.com/movies.json',{
+  method:'POST',
+  body:JSON.stringify(movie),
+  headers:{
+    'Content-Type ': 'application/json'
+
+  }
+})
+const data = await response.json();
+console.log(data)
 }
 
 
